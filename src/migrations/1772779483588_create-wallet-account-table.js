739f282e74a3c7404acbe0671fe11c17dcc7f600
riwayatOfficialRepo <riwayat.official.app@gmail.com>
2026-04-30 01:01:02 +0500

    remove obsolete migration for wallet account table

--- src/migrations/1772779483588_create-wallet-account-table.js
+++
@@ -1,4 +0,0 @@
-// No-op: some DBs recorded this name in pgmigrations before the file was reorganised.
-// Actual table creation is in 1773500000001. Restored so checkOrder does not fail.
-exports.up = (_pgm) => {};
-exports.down = (_pgm) => {};
