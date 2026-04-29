3d53f93bd79e406d69d02f0e0ea6d7ab99406e5f
sana4560 <sanawaqar4560@gmail.com>
2026-04-30 00:11:10 +0500

    add backfill script for rider roles and enhance database SSL configuration

--- src/migrations/1776426555711_add-col-kitchen-id-case-table.js
+++ src/migrations/1776426555711_add-col-kitchen-id-case-table.js
@@ -1,18 +1,16 @@
+/**
+ * Migration: Add kitchen_id column to cases table
+ */
 
-
-exports.up =  (pgm) => {
-  pgm.sql(
-   `DO $$ BEGIN
-    ALTER TABLE cases ADD COLUMN kitchen_id UUID;
+exports.up = (pgm) => {
+  pgm.sql(`
+    DO $$ BEGIN
+      ALTER TABLE cases ADD COLUMN kitchen_id UUID;
     EXCEPTION WHEN duplicate_column THEN NULL;
     END $$;
   `);
 };
 
-exports.down =  (pgm) => {
-  pgm.sql(`DO $$ BEGIN
-        ALTER TABLE cases DROP COLUMN IF EXISTS kitchen_id;
-        EXCEPTION WHEN duplicate_column THEN NULL;
-        END $$;
-    `)
+exports.down = (pgm) => {
+  pgm.sql(`ALTER TABLE cases DROP COLUMN IF EXISTS kitchen_id`);
 };
