df37c64b04a547b9bd0b10ee79b24a4a96c0d2f9
sana4560 <sanawaqar4560@gmail.com>
2026-07-02 13:00:10 +0500

    Update refresh_tokens user_type constraint to include DELIVERY_DRIVER

---
+++ src/migrations/1779850000031_update-refresh-tokens-user-type-constraint.js
@@ -0,0 +1,17 @@
+exports.up = (pgm) => {
+  pgm.sql(`ALTER TABLE refresh_tokens DROP CONSTRAINT IF EXISTS chk_refresh_tokens_user_type`);
+  pgm.sql(`
+    ALTER TABLE refresh_tokens
+      ADD CONSTRAINT chk_refresh_tokens_user_type
+      CHECK (user_type IN ('PARTNER', 'TEAM', 'CUSTOMER', 'DELIVERY_DRIVER'))
+  `);
+};
+
+exports.down = (pgm) => {
+  pgm.sql(`ALTER TABLE refresh_tokens DROP CONSTRAINT IF EXISTS chk_refresh_tokens_user_type`);
+  pgm.sql(`
+    ALTER TABLE refresh_tokens
+      ADD CONSTRAINT chk_refresh_tokens_user_type
+      CHECK (user_type IN ('PARTNER', 'TEAM', 'CUSTOMER'))
+  `);
+};
