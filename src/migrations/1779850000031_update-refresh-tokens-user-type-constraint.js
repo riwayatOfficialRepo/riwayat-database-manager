88258d3e5be1255060bcecab6cdbb2f6980e1387
sana4560 <sanawaqar4560@gmail.com>
2026-07-03 07:57:28 +0500

    Use NOT VALID and exception handling for refresh_tokens user_type constraint

--- src/migrations/1779850000031_update-refresh-tokens-user-type-constraint.js
+++ src/migrations/1779850000031_update-refresh-tokens-user-type-constraint.js
@@ -1,9 +1,12 @@
 exports.up = (pgm) => {
   pgm.sql(`ALTER TABLE refresh_tokens DROP CONSTRAINT IF EXISTS chk_refresh_tokens_user_type`);
   pgm.sql(`
-    ALTER TABLE refresh_tokens
-      ADD CONSTRAINT chk_refresh_tokens_user_type
-      CHECK (user_type IN ('PARTNER', 'ADMIN', 'TEAM', 'CUSTOMER', 'DELIVERY_DRIVER', 'RIDER'))
+    DO $$ BEGIN
+      ALTER TABLE refresh_tokens
+        ADD CONSTRAINT chk_refresh_tokens_user_type
+        CHECK (user_type IN ('PARTNER', 'ADMIN', 'TEAM', 'CUSTOMER', 'DELIVERY_DRIVER', 'RIDER')) NOT VALID;
+    EXCEPTION WHEN duplicate_object THEN NULL;
+    END $$;
   `);
 };
 
